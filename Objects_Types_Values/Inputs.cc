967e252cd60e574160d6aeb06393aace11dc4e3c
Sumit Dahiya <sumit.dahiya@ansys.com>
2022-08-28 00:44:19 +0530

    Chapter 3 : Objects_Types_Values

---
+++ Objects_Types_Values/Inputs.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main () {
+    string newname;
+    
+    cout<<"Enter a name and press enter\n";
+    
+    cin>>newname;
+    cout<<newname<<"\n";
+
+    int age;
+    cin>>newname>>age;
+    cout<<newname<<" "<<"age : "<<age<<endl;
+    return 0;
+}
