67ce633daf258418e8faabb364ecbc4fd23cb2d1
Sumit Dahiya <sumit.dahiya@ansys.com>
2022-09-01 20:30:10 +0530

    Added Compilation Process from preprocessor to compiler to assembler to linker to executable(will be used by loader)

---
+++ Compiler_Process/prog.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+#define y 20
+#define area(x,y) (x*y) // there should be no space between area(x,y), like area (x,y), does not work like that
+
+/*
+for preprocessor file : it will have all the declaration, definition will be included at linking time, binary object file
+g++ -E .\prog.cc > preprocessor.ii // intermediate/temporary file
+
+g++ -S .\prog.cc // from compiler , for assembly code, to be used by Assembler, by default file generated will be <name.s>,
+if .cc file name is <name.cc>
+
+
+g++ -c .\prog.cc // for object file, binary file will be used by linker to create executable
+*/
+
+int main () {
+
+    int x{1};
+
+    cout<<x+y<<"\n";
+    cout<<area(x,y)<<"\n";
+
+    return 0;
+}
